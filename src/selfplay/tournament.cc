7e5ce4dba2251fdf1512138c33d552517767c7c0
lucario6607 <bowlingkings@icloud.com>
2025-05-15 19:07:56 -0500

    Update tournament.cc

--- src/selfplay/tournament.cc
+++ src/selfplay/tournament.cc
@@ -34,6 +34,7 @@
 #include "neural/memcache.h"
 #include "neural/shared_params.h"
 #include "search/classic/search.h"
+#include "selfplay/trainingdata.h"
 #include "search/classic/stoppers/factory.h"
 #include "selfplay/game.h"
 #include "selfplay/multigame.h"
